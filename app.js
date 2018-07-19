139338594d8d5c8fe7bb7f910e07164f5e5a7405
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-07-19 03:34:10 -0400

    got the spans mapped out

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var pokeId = document.getElementById('pokeId');
 var pokeName = document.getElementById('pokeName');
 var pokeHp = document.getElementById('pokeHp');
 var length = document.getElementById('length');
-var width = document.getElementById('width');
+var weight = document.getElementById('weight');
 var move = document.getElementsByClassName('move');
 // ======================================================
 // POKECARD REVEAL/HIDE
-document.getElementById('btn').addEventListener('click', reveal);
+document.getElementById('bttn').addEventListener('click', reveal);
 
 function reveal(pokeCard){
+  row = document.getElementById('rowShow');
+  row.classList.remove('row-hidden');
+  row.classList.add('row-reveal');
   pkcd = document.getElementById('pokeCard');
   pkcd.classList.remove('hidden');
   pkcd.classList.add('reveal');
