0d0480a9cbb7fd7e6c059fc6eb3ddb107b39406e
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-07-20 09:36:19 -0400

    things

--- ohboy.js
+++ ohboy.js
@@ -1,8 +1,15 @@
 trainer = {
-  name: name;
+  name: name,
   pokemon: []
 }
 
 pokemon = {
-  
+  id: id,
+  name: name,
+  hp: hp,
+  species: species,
+  length: length,
+  weight: weight,
+  move: [],
+  abil: []
 }
