1c78f749b2e81e02feeaa35325fe63c64ae4dd9d
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-07-20 11:15:15 -0400

    maybe im doing something

--- ohboy.js
+++ ohboy.js
@@ -1,15 +1,42 @@
-trainer = {
-  name: name,
-  pokemon: []
+console.log('JavaScript is enabled!');
+class Trainer {
+  constructor(pokemon) {
+    this.name = name;
+    this.pokemon = pokemon;
+  }
 }
 
-pokemon = {
-  id: id,
-  name: name,
-  hp: hp,
-  species: species,
-  length: length,
-  weight: weight,
-  move: [],
-  abil: []
+changeCard(card){
+  this.name = card.constructor.name
+}
+
+class Pokemon extends Trainer{
+  constructor(id, name, hp, stats, move, ability){
+    super();
+    this.id = id;
+    this.name = name;
+    this.hp = hp;
+    this.stats = {species: species, length: length, weight: weight};
+    this.move = [];
+    this ability = [];
+  }
+}
+
+var poke1 = ;
+
+function loadPokemon(pokemon) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      poke1 = JSON.parse(this.responseText);
+      console.log(poke1);
+      poke1 = new pokemon()
+      document.getElementById("cardTop").innerHTML = poke1.name;
+    }
+  };
+
+
+
+  xhttp.open("GET", "http://pokeapi.co/api/v2/pokemon/" + id, true);
+  xhttp.send();
 }
