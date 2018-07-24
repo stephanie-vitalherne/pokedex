47a2c19a14293aa236f302e0368669a400034369
Kayler C <kmcookhorn@gmail.com>
2018-07-24 16:07:22 -0400

    grid overlay added to css file for data input over card images

--- bjb.js
+++ bjb.js
@@ -17,7 +17,7 @@ function render(){
   document.getElementById('cardfront').src = 'images/palkia-card-empty.jpg';
   // document.getElementById('cardfront').classList.add('flipInY');
   // document.getElementById('pkecd').classList.remove('span-hidden');
-  document.getElementById('pokeId').innerHTML = jsonURL.id;
+  document.getElementById('pokeId').innerHTML = 'ID #: '+jsonURL.id;
  document.getElementById('pokeName').innerHTML = jsonURL.name;
  document.getElementById('pokeHp').innerHTML = jsonURL.stats[5].base_stat + ' HP';
  document.getElementById('move').innerHTML = jsonURL.moves[4].move.name;
